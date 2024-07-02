bd61c01fa349830f5de86df78c9ce16b596842b4
Marcelo <marcelo.silva@sefin.fortaleza.ce.gov.br>
2024-07-02 11:49:32 -0300

    terminando layout

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module PortfolioJm
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.paths << Rails.root.join('lib', 'assets')
+
   end
 end
